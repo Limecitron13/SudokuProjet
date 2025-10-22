81691b1f9b24d5882ac7cd357650f5d9ea29e151
Limecitron13 <emlal31@ulaval.ca>
2025-10-21 20:13:40 -0400

    Ajout des fichiers resoudre.cpp et resoudre.h

---
+++ resoudre.cpp
@@ -0,0 +1,7 @@
+/***
+ * \file resoudre.cpp
+ * \author Émil Lavoie-Leullier
+ * \date 2025-10-21
+ * \brief ***TODO***
+ */
+
---
+++ resoudre.h
@@ -0,0 +1,14 @@
+/***
+ * \file resoudre.h
+ * \author Émil Lavoie-Leullier
+ * \date 2025-10-21
+ * \brief ***TODO***
+ */
+
+#ifndef RESOUDRE_H
+#define RESOUDRE_H
+
+
+
+#endif /* RESOUDRE_H */
+
