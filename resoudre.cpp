edf01bd0343cda410da9115cc73fc6677f6a3815
Limecitron13 <emlal31@ulaval.ca>
2025-10-22 13:26:23 -0400

    Ajout des prototypes de la fonction resoudre dans le .cpp et .h

--- resoudre.cpp
+++ resoudre.cpp
@@ -5,3 +5,17 @@
  * \brief ***TODO***
  */
 
+#include "Grille.h"
+using namespace std;
+
+
+
+/***
+ * \brief     ***TODO***
+ * \param g est un objet Grille qui représente la grille de sudoku à résoudre
+ * \return La grille résolue (une des solutions possibles à partir des conditions initiales)
+ */
+const Grille& resoudre(Grille& g)
+{
+    
+}
--- resoudre.h
+++ resoudre.h
@@ -4,11 +4,12 @@
  * \date 2025-10-21
  * \brief ***TODO***
  */
+#include "Grille.h"
 
 #ifndef RESOUDRE_H
 #define RESOUDRE_H
 
-
+const Grille& resoudre(Grille&);
 
 #endif /* RESOUDRE_H */
 
